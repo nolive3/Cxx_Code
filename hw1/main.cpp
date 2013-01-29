e580f937d175f68901bb046d1d2564c971c4696d
Nick <nolive3@gatech.edu>
2013-01-28 22:06:40 -0500

    MAIN

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -1,4 +1,13 @@
+#include "calculator.h"
+#include "scanner.h"
+#include <iostream>
 
 int main(void){
+    Scanner scan{std::cin};
+    Calculator calc{scan};
+    std::cout<<"Welcome to the High Order Calculator (q to quit)"<<std::endl;
+    while(calc.run(std::cout)){
+
+    }
     return 0;
 }
