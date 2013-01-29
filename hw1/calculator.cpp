0982150e6f5e03e9c69081f4cb0e22e17bdc71ae
Nick <nolive3@gatech.edu>
2013-01-29 18:19:15 -0500

    switched to left associativity
    this removes the right associativity feature introduced in a previous
    commit

--- hw1/calculator.cpp
+++ hw1/calculator.cpp
@@ -16,10 +16,15 @@ bool Calculator::run(std::ostream& output)
     output<<"Enter a formula: ";
     Token res = expr();
     if(res.valid()){
-        output<<"The answer is: "<<res.value()<<std::endl;
+        Token END = scan_stream.getNextToken();
+        if(END.type()==Token::EOL)
+            output<<"The answer is: "<<res.value()<<std::endl;
+        else
+            output<<"Valid input was followed by garbage!"<<std::endl;
+        scan_stream.unget(END);
     }else{
         Token END = scan_stream.getNextToken();
-        if(END.type()=="END"){
+        if(END.type()==Token::END){
             output<<"Goodbye!"<<std::endl;
             return false;
         }else{
@@ -28,7 +33,7 @@ bool Calculator::run(std::ostream& output)
         scan_stream.unget(END);
     }
     Token clr;
-    while((clr=scan_stream.getNextToken()).type()!="EOL"){
+    while((clr=scan_stream.getNextToken()).type()!=Token::EOL){
     }
     return true;
 }
@@ -36,96 +41,82 @@ bool Calculator::run(std::ostream& output)
 
 Token Calculator::expr()
 {
+    Token E = Token(Token::EXPR);
     Token T = term();
     if(T.valid()){
-        Token A = addop();
-        if(A.valid()){
-            Token E = expr();
-            if(E.valid()){
-                Token END = scan_stream.getNextToken();
-                if(END.type() == "EOL"){
-                    std::stringstream slop;
-                    std::stringstream srop;
-                    std::stringstream sres;
-                    slop<<T.value();
-                    double lop;
-                    slop>>lop;
-                    srop<<E.value();
-                    double rop;
-                    srop>>rop;
-                    if(A.value()=="+"){
-                        sres<<lop+rop;
-                    }else{
-                        sres<<lop-rop;
-                    }
-                    std::string res;
-                    sres>>res;
-                    Token exp("EXPR", res);
-                    scan_stream.unget(END);
-                    return exp;
-                }else{
-                    scan_stream.unget(END);
-                    scan_stream.unget(E);
+            double lhs;
+            std::string res;
+            std::stringstream lhs_con;
+            lhs_con<<T.value();
+            lhs_con>>lhs;
+            E = T.value();
+            Token A = addop();
+            while(A.valid()){
+                std::stringstream res_con;
+                std::stringstream rhs_con;
+                double rhs;
+                T = term();
+                if(!T.valid()){
                     scan_stream.unget(A);
-                    scan_stream.unget(T);
+                    return E;
                 }
-            }else{
-                scan_stream.unget(A);
-                scan_stream.unget(T);
+                rhs_con<<T.value();
+                rhs_con>>rhs;
+                if(A.value() == "+"){
+                    lhs+=rhs;
+                }else{
+                    lhs-=rhs;
+                }
+                res_con<<lhs;
+                res_con>>res;
+                E = res;
+                A = addop();
             }
-        }else{
-            Token END = scan_stream.getNextToken();
-            if(END.type() == "EOL"){
-                scan_stream.unget(END);
-                return T;
-            }else{
-                scan_stream.unget(END);
-                scan_stream.unget(T);
-            }
-        }
+            return E;
     }
     return Token();
 }
 Token Calculator::term()
 {
+    Token T = Token(Token::TERM);
     Token F = factor();
     if(F.valid()){
-        Token M = mulop();
-        if(M.valid()){
-            Token T = term();
-            if(T.valid()){
-                std::stringstream slop;
-                std::stringstream srop;
-                std::stringstream sres;
-                slop<<F.value();
-                double lop;
-                slop>>lop;
-                srop<<T.value();
-                double rop;
-                srop>>rop;
-                if(M.value()=="*"){
-                    sres<<lop*rop;
-                }else{
-                    sres<<lop/rop;
+            double lhs;
+            std::string res;
+            std::stringstream lhs_con;
+            lhs_con<<F.value();
+            lhs_con>>lhs;
+            T = F.value();
+            Token M = mulop();
+            while(M.valid()){
+                std::stringstream res_con;
+                std::stringstream rhs_con;
+                double rhs;
+                F = factor();
+                if(!F.valid()){
+                    scan_stream.unget(M);
+                    return T;
                 }
-                std::string res;
-                sres>>res;
-                Token ter("TERM", res);
-                return ter;
-            }else{
-                scan_stream.unget(M);
-                scan_stream.unget(F);
+                rhs_con<<F.value();
+                rhs_con>>rhs;
+                if(M.value() == "*"){
+                    lhs*=rhs;
+                }else{
+                    lhs/=rhs;
+                }
+                res_con<<lhs;
+                res_con>>res;
+                T = res;
+                M = mulop();
             }
-        }else{
-            return F;
-        }
+            return T;
     }
     return Token();
 }
 Token Calculator::factor()
 {
     Token N = scan_stream.getNextToken();
-    if(N.type()=="NUMBER"){
+    if(N.type()==Token::NUMBER){
         return N;
     }else{
         scan_stream.unget(N);
@@ -135,7 +126,7 @@ Token Calculator::factor()
 Token Calculator::mulop()
 {
     Token M = scan_stream.getNextToken();
-    if(M.type()=="MULTOP"){
+    if(M.type()==Token::MULTOP){
         return M;
     }else{
         scan_stream.unget(M);
@@ -145,7 +136,7 @@ Token Calculator::mulop()
 Token Calculator::addop()
 {
     Token A = scan_stream.getNextToken();
-    if(A.type()=="ADDOP"){
+    if(A.type()==Token::ADDOP){
         return A;
     }else{
         scan_stream.unget(A);
